968df1d7e2cde073aa5523a0ac16a5e55fbfcdc3
maraujodev <miguelaraujovieira7@gmail.com>
2021-06-07 12:07:44 -0300

    Primeiros comandos com o python

--- Aula4.py
+++ Aula4.py
@@ -3,3 +3,4 @@ nome = str(input('Digite o seu nome:'))
 idade = int(input('Digite a sua idade:'))
 peso = float(input('Digite o seu peso:'))
 print('Olá {} sua idade e {} seu peso e {}'.format(name).)
+
