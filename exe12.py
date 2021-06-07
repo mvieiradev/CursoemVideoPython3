3c74f0d63a2fe3f5432074a2932af3537fb0e1c4
maraujodev <miguelaraujovieira7@gmail.com>
2021-06-07 12:40:27 -0300

    Calculando Desconto

--- exe12.py
+++ exe12.py
@@ -1,3 +1,4 @@
+#Calculando Desconto
 preço = float(input('Qual e o preço do produto? R$'))
 novo = preço - (preço * 5 / 100)
 #print('O produto que custava R${}, na promoçao com deconto de 5% vai custar R${}'.format(preço, novo))
