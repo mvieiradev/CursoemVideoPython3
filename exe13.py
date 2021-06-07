0c59656f9be48390f3516c9be80e7f9eda6054d8
maraujodev <miguelaraujovieira7@gmail.com>
2021-06-07 12:41:14 -0300

    Reajustando Descontos

--- exe13.py
+++ exe13.py
@@ -1,3 +1,4 @@
+#Reajustando Descontos
 salario = float(input('Qual e o salario do funcionario? R$'))
 novo = salario + (salario * 15 / 100)
 print('Um funcionatio que ganhava R${}, com 15% de aumento, passa a receber R${}'.format(salario, novo))
