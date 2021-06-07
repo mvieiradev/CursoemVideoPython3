73dbda7aeb1e8dbf3716edb99a656f97bf1586ac
maraujodev <miguelaraujovieira7@gmail.com>
2021-06-07 13:31:59 -0300

    Custo de Viagen

--- exe31.py
+++ exe31.py
@@ -1,3 +1,4 @@
+#Custo de Viagen
 distancia = float(input('Quale a distancia da sua viagem?'))
 print('Voce esta prestes a comecar uma viagen de {}Km.'.format(distancia))
 preço = distancia * 0.50 if distancia <= 200 else distancia * 0.45
