c74bddfb92578f4b09925db41887cf8f96ff1ca7
maraujodev <miguelaraujovieira7@gmail.com>
2021-06-07 12:22:03 -0300

    Cores no terminal

--- aula11.py
+++ aula11.py
@@ -1,3 +1,4 @@
+#Cores no terminal
 nome = 'Miguel'
 cores = {'limpa':'\033[m', 'azul':'\033[33m', 'pretorbranco':'\033[7:30m'}
 print('Ola que prezer em te conhecer, {}{}{}!!'.format('\033[7:36m', nome, '\033[m'))
