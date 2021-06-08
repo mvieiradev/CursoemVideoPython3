a370f3a92e7b17baa70140703958149c365af497
maraujodev <miguelaraujovieira7@gmail.com>
2021-06-08 17:55:39 -0300

    Conversor de Bases Numerais

--- exe38.py
+++ exe38.py
@@ -1,1 +1,9 @@
-#Comparando Números
+#Comparando Números]
+n1 = int(input('Primeiro numero'))
+n2 = int(input('Segundo numero'))
+if n1 > n2:
+    print('O PRIMEIRO valor e maior')
+elif n2 > n1:
+    print('O SEGUNDO valor e maior')
+else:
+    print('Os dois valores sao iguais')
