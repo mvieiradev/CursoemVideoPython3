4d4fb647c83a3bd024857bee125b5841f582d1cc
maraujodev <miguelaraujovieira7@gmail.com>
2021-06-21 18:44:12 -0300

    Alistamento militar

---
+++ exe40.py
@@ -0,0 +1,10 @@
+nota1 = float(input('Primeira nota: '))
+nota2 = float(input('Segunda nota: '))
+media = (nota1 + nota2) /2
+print('tirando {:.1f} e {:.1f} a media do aluno e {:.1f}'.format(nota1, nota2, media))
+if 7 > media >= 5:
+    print('O aluno esta em recuperaçao')
+elif a media >= 5:
+    print('O aluno esta reprovado')
+elif media >= 7 :
+    print('O aluno esta aprovado')
