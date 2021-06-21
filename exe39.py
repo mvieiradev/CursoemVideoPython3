12bfe3133f2e2a89f227f97a2ce3c93bc4fdb277
maraujodev <miguelaraujovieira7@gmail.com>
2021-06-21 18:37:36 -0300

    Alistamento militar

--- exe39.py
+++ exe39.py
@@ -1,1 +1,18 @@
 #Alistamento Militar
+from datetime import date
+atual = date.today().year
+nasc = int(input('Ano de nascimento: '))
+idade = atual - nasc
+print('Quem nasceu em {} tem {} anos em {}.'.format(nasc, idade, atual))
+if idade == 18:
+    print('voce tem que se alistar IMEDIATAMENTE')
+elif idade < 18:
+    soldo = 18 - idade
+    print('Ainda faltam {} para o alistamento'.format(soldo))
+    ano = atual + soldo
+    print('Seu alistamento sera em {}'.format(ano))
+elif idade > 18:
+    soldo = 18 - idade
+    print('voce deveria ter se alistado ha {} anos '.format(soldo))
+    ano = atual - soldo
+    print('Seu alistamento foi em {}'.format(ano))
